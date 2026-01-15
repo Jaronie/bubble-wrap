76405f8e9f55cddf7f98d2600b83cc35a61842e9
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-01-15 09:40:36 +0000

    Made boilerpoint. Background set to blue.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <bn_core.h>
+#include <bn_backdrop.h>
+#include <bn_color.h>
+
+int main()
+{
+
+    bn::core::init();
+    bn::backdrop::set_color(bn::color(20, 20, 31));
+
+    while (true)
+    {
+
+        bn::core::update();
+    }
+}
