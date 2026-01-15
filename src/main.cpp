dc104e164ebf75716784a5d0771043b23b4454ee
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-01-15 10:22:59 +0000

    Added a feature where if B is held down, the background color changes.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_keypad.h>
+#include <bn_green_swap.h>
 
 int main()
 {
@@ -20,6 +21,15 @@ int main()
         if (bn::keypad::b_pressed())
         {
             bn::backdrop::set_color(bn::color(3, 25, 3));
+        }
+
+        if (bn::keypad::b_held())
+        {
+            bn::backdrop::set_color(bn::color(3, 25, 3));
+        }
+        if (bn::keypad::b_released())
+        {
+            bn::backdrop::set_color(bn::color(20, 20, 31));
         }
 
         bn::core::update();
