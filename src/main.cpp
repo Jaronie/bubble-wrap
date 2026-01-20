e61f4f4724ae5e3823b7fdd40b468f5298f04cdd
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-01-20 07:03:54 +0000

    included BN_LOG

--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,33 @@ int main()
     bn::core::init();
     bn::backdrop::set_color(bn::color(20, 20, 31));
 
-    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(10, 40);
-    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(0, 40);
-    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-10, 40);
-    bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-20, 40);
-    bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(-30, 40);
+    /* smile
+     bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(10, 40);
+     bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(0, 40);
+     bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-10, 40);
+     bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-20, 40);
+     bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(-30, 40);
 
-    //left side of smile
-    bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(-40, 30);
+     //left side of smile
+     bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(-40, 30);
 
-    //right side of smile
-        bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(20, 30);
+     //right side of smile
+         bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(20, 30);
 
-        //eyes
+         //eyes
 
-        //right eye
-            bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(0, 10);
+         //right eye
+             bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(0, 10);
 
-        //left eye
-            bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(-20, 10);
+         //left eye
+             bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(-20, 10);
+
+             */
+
+    for (int x = -40; x <= 40; x += 10)
+    {
+        bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(x, 40);
+    }
 
     while (true)
     {
